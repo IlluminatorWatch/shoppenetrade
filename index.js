a4b71aaa7a09a9155e01460a2c2890d3d6711734
IlluminatorWatch <58415047+IlluminatorWatch@users.noreply.github.com>
2020-02-26 12:50:37 +0100

    workshop init

---
+++ index.js
@@ -0,0 +1,24 @@
+// Härifrån startar vi upp vår webshop
+const mongoose = require('mongoose')
+const dbUrl = process.env.MONGO_ATLAS_URL || require("./config/config").databaseURL
+const { app, port } = require('./src/server')
+
+// const dbConfig = require('./config/config')
+// // if(dbConfig.databaseURL == undefined) {
+// //     dbConfig.databaseURL == "<API för miljövaribabel netlify>"
+// // }
+
+// if (process.env.NODE_ENV == "production") {
+//     // Environment om/när vi är i produktion
+//     dbConfig.databaseURL = process.env.MONGO_ATLAS_URL // Värdet hämtas från miljövariabel i heroku
+// } else {
+//     const dbConfig = require("./config/config")
+// }
+
+// Kicka igång servern
+const dbOptions = { useUnifiedTopology: true, useNewUrlParser: true }
+mongoose.connect(dbUrl, dbOptions).then(() => {
+    app.listen(port, () => console.log(`App listening on port ${port}!`))
+})
+
+module.exports = { app, port }
